668cf4ca015935fd2827917638218b2b232bb3d1
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-05-04 13:59:26 +0200

    overwrite existing json

--- server.php
+++ server.php
@@ -8,7 +8,7 @@ $json_text = file_get_contents('./disks.json');
 
 //^ Decodifico in PHP
 
-$disks = json_decode($json_text);
+$disks = json_decode($json_text, true);
 
 // var_dump($disks);
 
@@ -19,17 +19,25 @@ $_POST['artist'];
 $_POST['genre'];
 $_POST['year'];
 
-var_dump($_POST['title']);
-var_dump($_POST['artist']);
-var_dump($_POST['genre']);
-var_dump($_POST['year']);
+// var_dump($_POST['title']);
+// var_dump($_POST['artist']);
+// var_dump($_POST['genre']);
+// var_dump($_POST['year']);
 
-$disks[] = [
-$_POST['title'],
-$_POST['artist'],
-$_POST['genre'],
-$_POST['year']
+$disks[] = ["titolo" => $_POST['title'],
+"artista" => $_POST['artist'],
+"genere" => $_POST['genre'],
+"anno" => (int) $_POST['year']
 ];
 
 var_dump($disks);
+
+//^ Codifico il PHP in json
+
+$text_json = json_encode($disks);
+var_dump($text_json);
+
+//^ Sovrascrivo il file json
+
+$json_update = file_put_contents('./disks.json', $text_json);
 ?>
